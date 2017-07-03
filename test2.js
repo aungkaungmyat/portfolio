e55c36a1a8da1e7e2aace4ff222477817239880a
aungkaungmyat <amyat@purdue.edu>
2017-07-03 13:00:17 +0630

    got hover caption to work

--- test2.js
+++ test2.js
@@ -1,65 +1,81 @@
-/**
- * menu.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
-;( function( window ) {
+/** Used Only For Touch Devices **/
+( function( window ) {
 
-	'use strict';
+	// for touch devices: add class cs-hover to the figures when touching the items
+	if( Modernizr.touch ) {
 
-	function extend( a, b ) {
-		for( var key in b ) {
-			if( b.hasOwnProperty( key ) ) {
-				a[key] = b[key];
-			}
+		// classie.js https://github.com/desandro/classie/blob/master/classie.js
+		// class helper functions from bonzo https://github.com/ded/bonzo
+
+		function classReg( className ) {
+			return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 		}
-		return a;
-	}
 
-	function DotNav( el, options ) {
-		this.nav = el;
-		this.options = extend( {}, this.options );
-  		extend( this.options, options );
-  		this._init();
-	}
+		// classList support for class management
+		// altho to be fair, the api sucks because it won't accept multiple classes at once
+		var hasClass, addClass, removeClass;
 
-	DotNav.prototype.options = {};
-
-	DotNav.prototype._init = function() {
-		// special case "dotstyle-hop"
-		var hop = this.nav.parentNode.className.indexOf( 'dotstyle-hop' ) !== -1;
-
-		var dots = [].slice.call( this.nav.querySelectorAll( 'li' ) ), current = 0, self = this;
-
-		dots.forEach( function( dot, idx ) {
-			dot.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				if( idx !== current ) {
-					dots[ current ].className = '';
-
-					// special case
-					if( hop && idx < current ) {
-						dot.className += ' current-from-right';
-					}
-
-					setTimeout( function() {
-						dot.className += ' current';
-						current = idx;
-						if( typeof self.options.callback === 'function' ) {
-							self.options.callback( current );
-						}
-					}, 25 );
+		if ( 'classList' in document.documentElement ) {
+			hasClass = function( elem, c ) {
+				return elem.classList.contains( c );
+			};
+			addClass = function( elem, c ) {
+				elem.classList.add( c );
+			};
+			removeClass = function( elem, c ) {
+				elem.classList.remove( c );
+			};
+		}
+		else {
+			hasClass = function( elem, c ) {
+				return classReg( c ).test( elem.className );
+			};
+			addClass = function( elem, c ) {
+				if ( !hasClass( elem, c ) ) {
+						elem.className = elem.className + ' ' + c;
 				}
-			} );
-		} );
-	}
+			};
+			removeClass = function( elem, c ) {
+				elem.className = elem.className.replace( classReg( c ), ' ' );
+			};
+		}
 
-	// add to global namespace
-	window.DotNav = DotNav;
+		function toggleClass( elem, c ) {
+			var fn = hasClass( elem, c ) ? removeClass : addClass;
+			fn( elem, c );
+		}
+
+		var classie = {
+			// full names
+			hasClass: hasClass,
+			addClass: addClass,
+			removeClass: removeClass,
+			toggleClass: toggleClass,
+			// short names
+			has: hasClass,
+			add: addClass,
+			remove: removeClass,
+			toggle: toggleClass
+		};
+
+		// transport
+		if ( typeof define === 'function' && define.amd ) {
+			// AMD
+			define( classie );
+		} else {
+			// browser global
+			window.classie = classie;
+		}
+
+		[].slice.call( document.querySelectorAll( 'ul.grid > li > figure' ) ).forEach( function( el, i ) {
+			el.querySelector( 'figcaption > a' ).addEventListener( 'touchstart', function(e) {
+				e.stopPropagation();
+			}, false );
+			el.addEventListener( 'touchstart', function(e) {
+				classie.toggle( this, 'cs-hover' );
+			}, false );
+		} );
+
+	}
 
 })( window );
