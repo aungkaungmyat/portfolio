30d2a626e5881bf4d67966dfe50f2f371c093aa2
aungkaungmyat <amyat@purdue.edu>
2017-07-04 20:54:03 +0630

    made files organized

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+$('#fullpage').fullpage({
+    //Navigation
+		// menu: '#menu',
+		// lockAnchors: false,
+		// anchors:['firstPage', 'secondPage'],
+		// navigation: false,
+		// navigationPosition: 'right',
+		// navigationTooltips: ['firstSlide', 'secondSlide'],
+		// showActiveTooltip: false,
+		// slidesNavigation: false,
+		// slidesNavPosition: 'bottom',
+        // anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        navigation: true,
+        navigationTooltips: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        // showActiveTooltip: true,
+        navigationPosition: 'right',
+	    menu: '#myMenu'
+});
+});
