ed093da8b2dd44405c1ba6b87195e66b1805d590
aungkaungmyat <amyat@purdue.edu>
2017-07-05 23:05:38 +0630

    cleared some mess for test1

--- test1.js
+++ test1.js
@@ -1,18 +1,7 @@
 jQuery(document).ready(function($){
-	//open/close lateral filter
-	$('.cd-filter-trigger').on('click', function(){
-		triggerFilter(true);
-	});
-	$('.cd-filter .cd-close').on('click', function(){
-		triggerFilter(false);
-	});
 
-	function triggerFilter($bool) {
-		var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
-		elementsToTrigger.each(function(){
-			$(this).toggleClass('filter-is-visible', $bool);
-		});
-	}
+
+
 
 	//mobile version - detect click event on filters tab
 	var filter_tab_placeholder = $('.cd-tab-filter .placeholder a'),
@@ -45,10 +34,7 @@ jQuery(document).ready(function($){
 		}
 	});
 
-	//close filter dropdown inside lateral .cd-filter
-	$('.cd-filter-block h4').on('click', function(){
-		$(this).toggleClass('closed').siblings('.cd-filter-content').slideToggle(300);
-	})
+
 
 	//fix lateral filter and gallery on scrolling
 	$(window).on('scroll', function(){
@@ -74,53 +60,17 @@ jQuery(document).ready(function($){
 	    controls: {
 	    	enable: false
 	    },
-	    callbacks: {
-	    	onMixStart: function(){
-	    		$('.cd-fail-message').fadeOut(200);
-	    	},
-	      	onMixFail: function(){
-	      		$('.cd-fail-message').fadeIn(200);
-	    	}
-	    }
+	    // callbacks: {
+	    // 	onMixStart: function(){
+	    // 		$('.cd-fail-message').fadeOut(200);
+	    // 	},
+	    //   	onMixFail: function(){
+	    //   		$('.cd-fail-message').fadeIn(200);
+	    // 	}
+	    // }
 	});
 
-	//search filtering
-	//credits http://codepen.io/edprats/pen/pzAdg
-	var inputText;
-	var $matching = $();
 
-	var delay = (function(){
-		var timer = 0;
-		return function(callback, ms){
-			clearTimeout (timer);
-		    timer = setTimeout(callback, ms);
-		};
-	})();
-
-	$(".cd-filter-content input[type='search']").keyup(function(){
-	  	// Delay function invoked to make sure user stopped typing
-	  	delay(function(){
-	    	inputText = $(".cd-filter-content input[type='search']").val().toLowerCase();
-	   		// Check to see if input field is empty
-	    	if ((inputText.length) > 0) {
-	      		$('.mix').each(function() {
-		        	var $this = $(this);
-
-		        	// add item to be filtered out if input text matches items inside the title
-		        	if($this.attr('class').toLowerCase().match(inputText)) {
-		          		$matching = $matching.add(this);
-		        	} else {
-		          		// removes any previously matched item
-		          		$matching = $matching.not(this);
-		        	}
-	      		});
-	      		$('.cd-gallery ul').mixItUp('filter', $matching);
-	    	} else {
-	      		// resets the filter to show all item if input is empty
-	      		$('.cd-gallery ul').mixItUp('filter', 'all');
-	    	}
-	  	}, 200 );
-	});
 });
 
 /*****************************************************
