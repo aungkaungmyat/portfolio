405bb371aff719f59337c1908b5d9c9a49f5305b
aungkaungmyat <amyat@purdue.edu>
2017-07-12 20:15:06 +0630

    fixed the  link issue

--- js/projects.js
+++ js/projects.js
@@ -197,7 +197,7 @@ $(document).ready(function($){
   title: 'Gathering App',
 	// text: "A finance management app which gives item recommendations based on one's spending habit and shows expected finance result on each usage.",
 	html:
-		"<p>bodyA Web App where you can gather people of same interest to do certain activities.</p>" +
+		"<p>A Web App where you can gather people of same interest to do certain activities.</p>" +
     '<a href="https://github.com/aungkaungmyat/GatheringApp"><i class="fa fa-github"></i><span> Github</span></a>'
 })
 	});
