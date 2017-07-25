0403dc97a21b8c13c14d6250b98ae688c9e8ca7e
aungkaungmyat <amyat@purdue.edu>
2017-07-25 14:27:47 +0630

    added animation for the whole contact page

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ $('#fullpage').fullpage({
                 if(anchorLink == 'fourthPage'){
 
    //    			alert("First slide loaded");
-                        $(this).find('.contact-topic').addClass("animated bounceInDown");
+                        $(this).find('#topicAnimation').addClass("animated fadeInDown");
                         $(this).find('#galaxy').addClass("animated rollIn");
-                        $(this).find('.contact-footer').addClass("animated bounceInUp");
+                        $(this).find('#footAnimation').addClass("animated fadeInUp");
                         $(this).find('#nameAnimation').addClass("animated lightSpeedIn");
                         $(this).find('#subjectAnimation').addClass("animated lightSpeedIn");
                         $(this).find('#emailAnimation').addClass("animated lightSpeedIn");
