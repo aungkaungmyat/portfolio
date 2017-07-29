c1aead16fa780b7f810350c2b139a05172b41de1
aungkaungmyat <amyat@purdue.edu>
2017-07-29 17:12:08 +0630

    added close click

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
+//when send button is clicked
   $(".sendMsgButton").click(function(){
     alert('clicked');
+  });
+
+//when chatbox is closed
+  $("#closeIcon").click(function(){
+    alert('close');
   })
 })
