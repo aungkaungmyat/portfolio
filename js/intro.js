fbbcdc7b69b920734c0e480cf3ca5cd97b8563ee
aungkaungmyat <amyat@purdue.edu>
2017-07-29 20:31:12 +0630

    made the chatbox disappered

--- js/intro.js
+++ js/intro.js
@@ -18,9 +18,12 @@ $(document).ready(function(){
     // console.log(this.className === "fa-window-minimize")
     if(this.className === "fa fa-window-minimize"){
     $('#minimizeIcon').addClass('fa-window-maximize').removeClass('fa-window-minimize');
+    $("#chatboxID").removeClass('hidechat');
+    
     }
     else{
     $('#minimizeIcon').addClass('fa-window-minimize').removeClass('fa-window-maximize');
+    $("#chatboxID").addClass('hidechat');
     }
   })
 })
