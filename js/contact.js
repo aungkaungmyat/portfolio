e04c08a03bb7f72f5d1d3fb403ee0f89af174208
aungkaungmyat <amyat@purdue.edu>
2017-08-18 14:11:59 -0400

    got sending mail to work

--- js/contact.js
+++ js/contact.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     window.open('mailto:aungkaungmyat.olaf@gmail.com'
     + "?subject=" + escape(document.getElementById('messageSubjectID').value)
     +  "&body=" + escape(document.getElementById('senderMessageID').value));
-    // console.log(document.getElementById('messageSubjectID').value)
   });
 
 })
