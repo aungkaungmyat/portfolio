56e4439eed746fef51a12ecf4a8fab34091fb74b
aungkaungmyat <amyat@purdue.edu>
2017-08-18 14:10:05 -0400

    got message to get to mail box

--- js/contact.js
+++ js/contact.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 
 
     window.open('mailto:aungkaungmyat.olaf@gmail.com'
-    + "?subject=" + escape(document.getElementById('messageSubjectID').value));
-    console.log(document.getElementById('messageSubjectID').value)
+    + "?subject=" + escape(document.getElementById('messageSubjectID').value)
+    +  "&body=" + escape(document.getElementById('senderMessageID').value));
+    // console.log(document.getElementById('messageSubjectID').value)
   });
 
 })
