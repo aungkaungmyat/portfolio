dd55ac9f49e843582619ead38722307f600f85c5
aungkaungmyat <amyat@purdue.edu>
2017-08-18 14:07:36 -0400

    got message subject to  work

--- js/contact.js
+++ js/contact.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $("#messageSubmit").click(function(){
 
 
-    window.open('mailto:aungkaungmyat.olaf@gmail.com');
+    window.open('mailto:aungkaungmyat.olaf@gmail.com'
+    + "?subject=" + escape(document.getElementById('messageSubjectID').value));
+    console.log(document.getElementById('messageSubjectID').value)
   });
+
 })
