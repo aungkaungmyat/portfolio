dbf7361145ef578acbe1647f337a3cfb2f2325e3
aungkaungmyat <amyat@purdue.edu>
2017-08-19 22:23:45 -0400

    started loading gif

--- js/main.js
+++ js/main.js
@@ -45,3 +45,17 @@ $('#fullpage').fullpage({
 
 });
 });
+
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+      //  document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+        //  document.getElementById('interactive');
+        //  document.getElementById('load').style.visibility="hidden";
+        //  document.getElementById('contents').style.visibility="visible";
+      },1000);
+  }
+}
